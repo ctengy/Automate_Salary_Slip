fd4f63bac0722b23b69c25cba0d4665d7598023f
Richard Yin <ctengy.work@gmail.com>
2023-04-14 16:28:17 +0800

    open password file&chose convert target

--- 薪資單生成.py
+++ 薪資單生成.py
@@ -1,9 +1,21 @@
 import openpyxl as opxl
 from docx import Document
 import os
+import io
+import msoffcrypto as mso
 import win32com.client as win32
-word = win32.Dispatch('Word.Application')
 
+def loadencrypted_excel(encrypted_filename:str):#有密碼的話
+
+    decrypted = io.BytesIO()
+    password = input('請輸入密碼\n')
+    with open(encrypted_filename, "rb") as f:
+        file = mso.OfficeFile(f)
+        file.load_key(password= password)  # Use password
+        file.decrypt(decrypted)
+    return decrypted   
+
+        
 def replace_word_text(word_file, old_text:str, new_text:str):#文字替換函數
     try:
         for paragraph in word_file.paragraphs:#填入所有表格中paragraphs的部分
@@ -39,37 +51,50 @@ def encrypt_files(file_path, password_dic:dict, name):#加密檔案
         print(name,'薪資單已加密完畢')
     except:
         print('加密失敗')    
+        raise
 
 try:#指定檔案位置
+    soure_file = input('轉換檔案名\n')
     path = os.path.dirname(__file__)#當前目錄
-    generate_path = os.path.join(path, '生成文件')
+    generate_path = os.path.join(path, f'{soure_file}生成文件')
     os.makedirs(generate_path, exist_ok=True)#如果沒有自動生成資料夾
     path_docx = os.path.join(path,'範例薪資單.docx')
-    path_xlsx = os.path.join(path,'薪資表.xlsx')
+    path_xlsx = os.path.join(path,f'{soure_file}.xlsx')
+    print(path_xlsx)
     path_password = os.path.join(path,'密碼表.xlsx')
 except:
     print('檢查資料夾中檔案是否正確')
     raise
+
 try:#載入檔案
     workbook = opxl.load_workbook(path_xlsx,data_only= True) #讀取WORKBOOK並且設定data_only= True，使公式格回傳計算後結果
     sheet = workbook['薪資總表'] #選擇SHEET
     paydate = str(sheet['A2'].value)[0:11]#從表格取得日期字串
 except:
-    print('讀取錯誤檢查檔案EXCEL檔案')
-    raise
-while True:
+    try:
+        decryptedwb = loadencrypted_excel('薪資表.xlsx')
+        workbook = opxl.load_workbook(decryptedwb,data_only= True) #讀取WORKBOOK並且設定data_only= True，使公式格回傳計算後結果
+        sheet = workbook['薪資總表'] #選擇SHEET
+        paydate = str(sheet['A2'].value)[0:11]
+    except:
+        print('讀取錯誤檢查檔案EXCEL檔案')
+        raise
+
+while True:#使用者指定資料
     try:
         star_row = int(input('薪資表表頭位置: '))
         end_row = int(input('人員名單結束位置: '))
         break
     except ValueError:
         print('請輸入整數！')
-try:
+
+try:#取值
     password = get_password(path_password)#取得密碼函數
+    word = win32.Dispatch('Word.Application')
     for table_row in range(star_row, end_row):#指定ROW循環範圍
         word_file = Document(path_docx)
         for table_col in range (3, sheet.max_column+1):#指定COL循環範圍
-            old_text = str(sheet.cell(row= star_row, column=table_col).value) #取得標題名當作範本WORD中的待替換文本
+            old_text = str(sheet.cell(row= star_row-1, column=table_col).value) #取得標題名當作範本WORD中的待替換文本
             new_value = sheet.cell(row=table_row, column=table_col).value  # 獲取欲替換單元格內容值
             new_text = str(new_value) if new_value not in [None, 0] else ""  # 將結果轉為字符串並且當結果為NONE或0時替換為空白
             replace_word_text(word_file,old_text,new_text)#調用替換函數
@@ -82,7 +107,7 @@ try:
         #加密薪資單
         path_unprotectdocx = os.path.join(generate_path, generate_file_name)#加密文件的路徑
         encrypt_files(path_unprotectdocx, password, name)#加密函數
-    word.Quit()#關掉wiv32模組
+    word.Quit()
     print('薪資單均已加密完畢')
 except:
     print('填入過程中出現錯誤')
